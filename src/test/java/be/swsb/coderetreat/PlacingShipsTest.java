3e4d40925c7435f9eea346610208e095f5404434
maaikedegreef <71584117+maaikedegreef@users.noreply.github.com>
2023-11-08 21:29:59 +0100

    Adjust test names

--- src/test/java/be/swsb/coderetreat/PlacingShipsTest.java
+++ src/test/java/be/swsb/coderetreat/PlacingShipsTest.java
@@ -8,14 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PlacingShipsTest {
     @Test
-    public void game_placingOneShipHorizontallyOnX1Y1_NumberOfBoatsEqualsToOne() {
+    public void placingShip_placingOneShipHorizontallyOnX1Y1_NumberOfBoatsEqualsToOne() {
         Game game = new Game(5);
         game.placeBoat(new Position(1, 1), Orientation.HORIZONTAL, BoatType.DESTROYER);
         assertEquals(game.getNumberOfBoats(), 1);
     }
 
     @Test
-    public void game_placingOneShipHorizontallyOnX1Y1_Visualisation() {
+    public void placingShip_placingOneShipHorizontallyOnX1Y1_Visualisation() {
         Game game = new Game(5);
         game.placeBoat(new Position(1, 1), Orientation.HORIZONTAL, BoatType.DESTROYER);
         assertEquals(game.render(), """
@@ -29,7 +29,7 @@ public class PlacingShipsTest {
     }
 
     @Test
-    public void game_placingOneShipHorizontallyOnX0Y1_Visualisation() {
+    public void placingShip_placingOneShipHorizontallyOnX0Y1_Visualisation() {
         Game game = new Game(5);
         game.placeBoat(new Position(0, 1), Orientation.HORIZONTAL, BoatType.DESTROYER);
         assertEquals(game.getNumberOfBoats(), 1);
@@ -44,7 +44,7 @@ public class PlacingShipsTest {
     }
 
     @Test
-    public void game_placingOneShipHorizontallyOnX2Y4_Visualisation() {
+    public void placingShip_placingOneShipHorizontallyOnX2Y4_Visualisation() {
         Game game = new Game(5);
         game.placeBoat(new Position(2, 4), Orientation.HORIZONTAL, BoatType.DESTROYER);
         assertEquals(game.getNumberOfBoats(), 1);
@@ -59,7 +59,7 @@ public class PlacingShipsTest {
     }
 
     @Test
-    public void game_placingOneShipVerticallyOnX1Y1_Visualisation() {
+    public void placingShip_placingOneShipVerticallyOnX1Y1_Visualisation() {
         Game game = new Game(5);
         game.placeBoat(new Position(1, 1), Orientation.VERTICAL, BoatType.DESTROYER);
         assertEquals(game.getNumberOfBoats(), 1);
@@ -74,7 +74,7 @@ public class PlacingShipsTest {
     }
 
     @Test
-    public void game_placingOneShipHorizontallyOnX4Y4_ThrowsError() {
+    public void placingShip_placingOneShipHorizontallyOnX4Y4_ThrowsError() {
         Game game = new Game(5);
         assertThrows(IllegalArgumentException.class, () -> {
             game.placeBoat(new Position(4, 4), Orientation.HORIZONTAL, BoatType.DESTROYER);
@@ -82,7 +82,7 @@ public class PlacingShipsTest {
     }
 
     @Test
-    public void game_placingOneShipHorizontallyOnX3Y2_ThrowsError() {
+    public void placingShip_placingOneShipHorizontallyOnX3Y2_ThrowsError() {
         Game game = new Game(5);
         assertThrows(IllegalArgumentException.class, () -> {
             game.placeBoat(new Position(3, 2), Orientation.HORIZONTAL, BoatType.DESTROYER);
@@ -90,7 +90,7 @@ public class PlacingShipsTest {
     }
 
     @Test
-    public void game_placingOneShipVerticallyOnX4Y4_ThrowsError() {
+    public void placingShip_placingOneShipVerticallyOnX4Y4_ThrowsError() {
         Game game = new Game(5);
         assertThrows(IllegalArgumentException.class, () -> {
             game.placeBoat(new Position(4, 4), Orientation.VERTICAL, BoatType.DESTROYER);
@@ -98,7 +98,7 @@ public class PlacingShipsTest {
     }
 
     @Test
-    public void game_placingOneShipVerticallyOnXmin3Y4_ThrowsError() {
+    public void placingShip_placingOneShipVerticallyOnXmin3Y4_ThrowsError() {
         Game game = new Game(5);
         assertThrows(IllegalArgumentException.class, () -> {
             game.placeBoat(new Position(-3, 4), Orientation.VERTICAL, BoatType.DESTROYER);
@@ -106,7 +106,7 @@ public class PlacingShipsTest {
     }
 
     @Test
-    public void game_placingOneCarrierHorizontallyOnX0Y0_Visualisation() {
+    public void placingShip_placingOneCarrierHorizontallyOnX0Y0_Visualisation() {
         Game game = new Game(5);
         game.placeBoat(new Position(0, 0), Orientation.HORIZONTAL, BoatType.CARRIER);
         assertEquals(game.getNumberOfBoats(), 1);
@@ -121,7 +121,7 @@ public class PlacingShipsTest {
     }
 
     @Test
-    public void game_placingOneBattleShipVerticallyOnX3Y0_Visualisation() {
+    public void placingShip_placingOneBattleShipVerticallyOnX3Y0_Visualisation() {
         Game game = new Game(5);
         game.placeBoat(new Position(3, 0), Orientation.VERTICAL, BoatType.BATTLESHIP);
         assertEquals(game.getNumberOfBoats(), 1);
@@ -136,7 +136,7 @@ public class PlacingShipsTest {
     }
 
     @Test
-    public void game_placingOneSubmarineVerticallyOnX3Y0_Visualisation() {
+    public void placingShip_placingOneSubmarineVerticallyOnX3Y0_Visualisation() {
         Game game = new Game(5);
         game.placeBoat(new Position(3, 0), Orientation.VERTICAL, BoatType.SUBMARINE);
         assertEquals(game.getNumberOfBoats(), 1);
@@ -151,7 +151,7 @@ public class PlacingShipsTest {
     }
 
     @Test
-    public void game_placingOnePatrolboatHorizontallyOnX0Y0_Visualisation() {
+    public void placingShip_placingOnePatrolboatHorizontallyOnX0Y0_Visualisation() {
         Game game = new Game(5);
         game.placeBoat(new Position(0, 0), Orientation.HORIZONTAL, BoatType.PATROLBOAT);
         assertEquals(game.getNumberOfBoats(), 1);
@@ -166,7 +166,7 @@ public class PlacingShipsTest {
     }
 
     @Test
-    public void game_placingTwoCarriersOnSamePosition_ThrowsError() {
+    public void placingShip_placingTwoCarriersOnSamePosition_ThrowsError() {
         Game game = new Game(5);
         game.placeBoat(new Position(0, 0), Orientation.HORIZONTAL, BoatType.CARRIER);
         assertThrows(IllegalStateException.class, () -> {
@@ -175,7 +175,7 @@ public class PlacingShipsTest {
     }
 
     @Test
-    public void game_placingTwoShipsOverlappingEachOther_ThrowsError() {
+    public void placingShip_placingTwoShipsOverlappingEachOther_ThrowsError() {
         Game game = new Game(5);
         game.placeBoat(new Position(2, 0), Orientation.VERTICAL, BoatType.BATTLESHIP);
         assertThrows(IllegalStateException.class, () -> {
